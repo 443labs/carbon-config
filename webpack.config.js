c91ab122f261df2d206276eaca114eea275c0b5f
Mark Belles <mark@disruptops.com>
2019-07-14 14:45:03 -0500

    Remove entries from webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: slsw.lib.entries,
   target: 'node',
   devtool: 'inline-source-map',
   mode: 'development',
